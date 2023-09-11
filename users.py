9e070d5cd919a6904a87d2959b27fd92f37c87f9
Philip Smith <philip@randomsmiths.com>
2023-09-11 12:21:08 -0400

    add helper methods and tests

--- users.py
+++ users.py
@@ -28,7 +28,6 @@ class Users:
         db_connection.execute("""
         CREATE TABLE Users(
             id INTEGER PRIMARY KEY,
-            displayName TEXT,
             firstName TEXT,
             lastName TEXT,
             email TEXT,
@@ -37,14 +36,43 @@ class Users:
             expectedGrad TEXT,
             agab TEXT
         )""")
-        
     
+    def add_user(self, db_connection:sqlite3.Connection, user: User) -> None:
+        db_connection.execute(
+            'INSERT INTO Users Values (?,?,?,?,?,?,?,?)', 
+            (user.ID, user.fname, user.lname, user.email, user.want_to_go_to_comp, user.class_status, 
+             user.expected_grad, user.agab,))
     
+    def turn_db_row_into_user(self, row: tuple):
+        return User(row[0], row[1], row[2], row[4], row[5], row[7], row[6], row[3])
         
+    def get_user_by_ID(self, db_connection: sqlite3.Connection, ID:int) -> tuple[str, User|None]:
+        data = db_connection.execute("SELECT * FROM Users WHERE id==?", (ID,)).fetchone()
+        if data:
+            return ('success', self.turn_db_row_into_user(data))
+        else:
+            return (f'Unknown ID {ID}', None)
         
-    
+    def get_all_users(self, db_connection: sqlite3.Connection) -> list[User]:
+        users = []
+        for row in db_connection.execute("SELECT * FROM Users"):
+            users.append(self.turn_db_row_into_user(row))
+        return users
 
 if __name__ == "__main__":
     DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'data/testDatabase.sqlite3')
+    users = Users()
+    
+    testUser = User(800000000, "philip", "smith", True, "sophmore", "Male", "Spring 2025", "psmit145@uncc.edu")
+    
+    with sqlite3.connect(DEFAULT_PATH) as connection:
+        try: 
+            users.create_table(connection)
+        except sqlite3.OperationalError:
+            connection.execute("DROP TABLE users")
+            users.create_table(connection)
+        users.add_user(connection, testUser)
+        print(users.get_user_by_ID(connection, 800000000))
+        print(users.get_all_users(connection))
     
     
